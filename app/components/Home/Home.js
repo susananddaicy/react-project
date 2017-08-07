f5c2816d05cfe90c694eef7b1bdc1d3820c65d48
daichunyu747 <daichunyu747@lu.com>
2017-08-07 11:05:22 +0800

    modify css and title

--- app/components/Home/Home.js
+++ app/components/Home/Home.js
@@ -15,7 +15,7 @@ class Home extends Component {
     return (
       <div className="Home">
         <div className="Home-container">
-        <p>React Components</p>
+        <h1>React Components</h1>
         <ul>
           <li><Link to="actionSheet">ActionSheet(动作面板)</Link></li>
           <li><Link to="accordion">Accordion(手风琴)</Link></li>    
