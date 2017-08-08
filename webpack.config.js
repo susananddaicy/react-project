76aefe07d2ff363146fe4c51f9f22573186a5c3b
daichunyu747 <daichunyu747@lu.com>
2017-08-08 10:23:30 +0800

    增加首页渲染速度

--- webpack.config.js
+++ webpack.config.js
@@ -25,8 +25,7 @@ var AUTOPREFIXER_BROWSERS = [
 
 module.exports= {
   entry: {
-    app: path.resolve(APP_PATH, 'app.js'),
-    vendors: ['babel-polyfill', 'react', 'react-dom', 'fastclick']
+    app: path.resolve(APP_PATH, 'app.js')
   },
   output: {
     path: BUILD_PATH,
@@ -35,6 +34,11 @@ module.exports= {
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
+  externals: [{
+    react: 'React',
+  }, {
+    'react-dom': 'ReactDOM',
+  }],
   //启动dev source map，出错以后就会采用source-map的形式直接显示你出错代码的位置。
   devtool: 'eval-source-map',
   //enable dev server
@@ -90,13 +94,13 @@ module.exports= {
     //这个使用uglifyJs压缩你的js代码
     new webpack.optimize.UglifyJsPlugin({minimize: true}),
     //把入口文件里面的数组打包成verdors.js
-    new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js'),
+    //new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js'),
     new ExtractTextPlugin('public/styles/react-component.css'), // 把css自动插入到head里
     new HtmlwebpackPlugin({ // 会把压缩好的app.js和vendors.js自动插入到html中的body里
       title: 'My first react app',
       template: path.resolve(APP_PATH, 'index.html'),
       filename: 'index.html',
-      chunks: ['app', 'vendors'],
+      chunks: ['app'],
       inject: 'body'
     })
   ],
